83197de2903ecef4ec9ed737850dc26090f9252b
haleykeizur <haleykz@yahoo.com>
2018-07-31 18:35:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,6 +4,15 @@ class App < Sinatra::Base
 get ‘/name’ do
 	"My name is Haley"
 end 
+
+get ‘/hometown’ do
+	"My hometown is Edgewood"
+end 
+
+get ‘/favorite-song’ do
+	"My favorite song is Change"
+end 
+
 end 
 
 
